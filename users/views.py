b93adcb9b4202a23e8ce56d0a397f82e2fd53373
Karol Michalak <karol.michalak93@gmail.com>
2019-04-04 13:51:57 +0200

    CRUD elements for blog posts

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(
-                request, f'Your account has been create, {username}!')
+                request, f'Your account has been created, {username}!')
             return redirect('login')
     else:
         form = UserRegisterForm()
