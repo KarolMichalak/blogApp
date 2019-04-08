45a1528eee977f1a00d32a19af294ca0f527efd2
Karol Michalak <karol.michalak93@gmail.com>
2019-04-08 20:08:17 +0200

    django-heroku config

--- djaBlog/settings.py
+++ djaBlog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,4 +143,10 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
+<<<<<<< HEAD
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+=======
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
+>>>>>>> 05e5b97... django-heroku config
