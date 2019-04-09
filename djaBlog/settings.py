1a273d3458469a9ac9a7a177339ad562572a65d4
Karol Michalak <karol.michalak93@gmail.com>
2019-04-09 15:21:19 +0200

    code cleanup

--- djaBlog/settings.py
+++ djaBlog/settings.py
@@ -119,9 +119,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
